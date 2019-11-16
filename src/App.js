93a0aea6e732324b16d9618d89da925d1b5317e2
Anthony Bassey <a.bassey@live.co.uk>
2019-11-16 14:18:12 +0100

    need picture for home page but besides that is complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from "react";
-import "./App.css";
+import React from "react"
+import './App.css'
 
-import HomePage from "./Component/HomePage";
+import HomePage from './Pages/homepage/homepage.component'
 
 function App() {
   return <div>
