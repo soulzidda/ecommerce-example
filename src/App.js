ff8c2382efa570977f9858ca064f509937ff5c20
Anthony Bassey <a.bassey@live.co.uk>
2019-11-17 22:26:54 +0100

    Added sign up page and all stylings so far also added dummy data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React from "react";
-import {Switch, Route} from 'react-router-dom'
+import React from "react"
+import { Switch, Route } from "react-router-dom"
 
-import "./App.css";
+import "./App.css"
 
 import HomePage from "./Pages/homepage/homepage.component"
 import ShopPage from "./Pages/shop/shop.component"
+import SignInAndSignUpPage from './Pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import Header from "../src/component/header/header.component"
 
 function App() {
-  return(
-  <div>
-    <Switch>
-    <Route exact path='/' component={HomePage} />
-    <Route path='/shop' component={ShopPage} />
-    </Switch>
-  </div>
-  )}
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path='/signup' component={SignInAndSignUpPage} />
+      </Switch>
+    </div>
+  );
+}
 
-export default App;
+export default App
