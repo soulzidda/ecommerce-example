c0cdfab9d969513fd49e2db93bfb24cb43a83f34
Anthony Bassey <a.bassey@live.co.uk>
2019-11-23 01:48:25 +0100

    completed checkout and added functionality

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -7,4 +7,14 @@ export const togglCartHidden = () => ({
 export const addItem = item => ({
   type: CartActionTypes.ADD_ITEM,
   payload: item
-})
+});
+
+export const removeItem = item => ({
+  type: CartActionTypes.REMOVE_ITEM,
+  payload: item
+});
+
+export const ClearItemFromCart = item => ({
+  type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+  payload: item
+});
--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,6 +1,8 @@
 const CartActionTypes = {
   TOGGLE_CART_HIDDEN: "TOGGLE_CART_HIDDEN",
-  ADD_ITEM: "ADD_ITEM"
+  ADD_ITEM: "ADD_ITEM",
+  REMOVE_ITEM: 'REMOVE_ITEM',
+  CLEAR_ITEM_FROM_CART: 'CLEAR-ITEM-FROM-CART'
 };
 
 export default CartActionTypes;
